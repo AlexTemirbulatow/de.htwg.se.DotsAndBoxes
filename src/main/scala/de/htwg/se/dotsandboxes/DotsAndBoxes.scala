d9e49d80291b775ff69ef3e4d8361e0fb60f05f6
Alextem <temirbulatow1@gmail.com>
2023-04-10 23:26:02 +0200

    now accessing model file to print field

--- src/main/scala/de/htwg/se/dotsandboxes/DotsAndBoxes.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/DotsAndBoxes.scala
@@ -1,14 +1,12 @@
-@main def dotsandboxes: Unit =
+package de.htwg.se.dotsandboxes
+
+import de.htwg.se.dotsandboxes.model.Filled
+import de.htwg.se.dotsandboxes.model.Matrix
+import de.htwg.se.dotsandboxes.model.Field
+
+
+@main def run: Unit =
   println("\nWelcome to Dots and Boxes\n")
-  print(mesh())
-
-def bar(lenght: Int = 6, cellNum: Int = 6) =
-  ("o" + "-" * lenght) * cellNum + "o" + "\n"
-
-def cells(lenght: Int = 6, cellNum: Int = 6, height: Int = 2) =
-  (("|" + " " * lenght) * cellNum + "|" + "\n") * height
-
-def mesh(lenght: Int = 6, cellNum: Int = 6, height: Int = 2) =
-  (bar(lenght, cellNum) + cells(lenght, cellNum, height)) * cellNum + bar(lenght, cellNum)
-
-
+  var field = new Field(5, 4, Filled.Empty)
+  println(field.toString)
+  print("\n")
