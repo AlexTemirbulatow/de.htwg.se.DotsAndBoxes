26b5ccc5d3d34710c4e216b5312fbede321c2980
Alextem <temirbulatow1@gmail.com>
2023-04-13 19:24:11 +0200

    updated file

--- src/main/scala/de/htwg/se/dotsandboxes/model/Player.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/model/Player.scala
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package de.htwg.se.dotsandboxes.model
 
 case class Player(name: String, points: Int = 0, filling: Filled)
@@ -6,18 +5,7 @@ case class Player(name: String, points: Int = 0, filling: Filled)
 val playerBlue = Player("Player Blue", 0, Filled.Blue)
 val playerRed = Player("Player Red", 0, Filled.Red)
 
-object Player:
+/*object Player:
   val list = List(playerBlue, playerRed)
   def next = list.iterator.next
-=======
-package de.htwg.se.dotsandboxes.model
-
-case class Player(name: String, points: Int = 0, filling: Filled)
-
-val playerBlue = Player("Player Blue", 0, Filled.Blue)
-val playerRed = Player("Player Red", 0, Filled.Red)
-
-//object Player:
-//  val list = List(playerBlue, playerRed)
-  //def next = list.iterator.next
->>>>>>> d9d1fb74d6cbf968da15f0fef726ca1c023e9c0b
+*/
