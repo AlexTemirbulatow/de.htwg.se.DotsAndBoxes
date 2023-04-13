bed503e35e0fba201119dffe96e1b545387d9a62
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-04-13 18:55:22 +0200

    add model

--- src/main/scala/de/htwg/se/dotsandboxes/model/Matrix.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/model/Matrix.scala
@@ -4,7 +4,8 @@ case class Matrix[T](vec: Vector[Vector[T]]):
   def this(row: Int, col: Int, filling: T) = this(Vector.tabulate(col, row) { (row, col) => filling})
   def cell(row: Int, col: Int): T = vec(row)(col)
   def row(row: Int) = vec(row)
-  def fill(filling: T): Matrix[T] = copy(Vector.tabulate(colSize, rowSize) { (row, col) => filling })
+  def fill(filling: T): Matrix[T] = copy(Vector.tabulate(colSizeEven, rowSize) { (row, col) => filling })
   def replaceCell(row: Int, col: Int, cell: T): Matrix[T] = copy(vec.updated(row, vec(row).updated(col, cell)))
   val rowSize: Int = vec.size
-  val colSize: Int = vec(0).size
+  val colSizeEven: Int = vec(0).size
+  //val colSizeOdd: Int = vec(1).size
