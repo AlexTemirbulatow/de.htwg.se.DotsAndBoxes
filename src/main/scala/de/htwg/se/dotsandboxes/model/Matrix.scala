0b36755d8c5369759182008c945bf278e2da8eb7
Alextem <temirbulatow1@gmail.com>
2023-04-18 20:38:12 +0200

    further development

--- src/main/scala/de/htwg/se/dotsandboxes/model/Matrix.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/model/Matrix.scala
@@ -1,15 +1,15 @@
 package de.htwg.se.dotsandboxes.model
 
-case class Matrix[T](vecFill: Vector[Vector[T]], vecX: Vector[Vector[Boolean]], vecY: Vector[Vector[Boolean]]):
+case class Matrix[T](vecFill: Vector[Vector[T]], vecX: Vector[Vector[Any]], vecY: Vector[Vector[Any]]):
   def this(row: Int, col: Int, filling: T) = 
     this(Vector.tabulate(col, row) { (_, _) => filling}, 
     Vector.tabulate (col + 1, row) { (_, _) => false}, 
     Vector.tabulate (col, row + 1) { (_, _) => false})
 
-  def cell(vecIndex: Int, row: Int, col: Int): T = this.productElement(vecIndex).asInstanceOf[Vector[Vector[T]]](row)(col)
-  def row(vecIndex: Int, row: Int) = this.productElement(vecIndex).asInstanceOf[Vector[Vector[T]]](row)
-  def replaceCell(vecIndex: Int, row: Int, col: Int, cell: T): Matrix[T] = 
-    copy(this.productElement(vecIndex).asInstanceOf[Vector[Vector[T]]].updated(
-    row, this.productElement(vecIndex).asInstanceOf[Vector[Vector[T]]](row).updated(col, cell)))
-  def rowSize(vecIndex: Int): Int = this.productElement(vecIndex).asInstanceOf[Vector[Vector[T]]].size
-  def colSize(vecIndex: Int, row: Int) = this.productElement(vecIndex).asInstanceOf[Vector[Vector[T]]](row).size
+  def cell(vecIndex: Int, row: Int, col: Int): Any = vector(vecIndex)(row)(col)
+  def row(vecIndex: Int, row: Int) = vector(vecIndex)(row)
+  def replaceCell(vecIndex: Int, row: Int, col: Int, cell: Any): Matrix[Any] = 
+    copy(vecX = vector(vecIndex).updated(row, vector(vecIndex)(row).updated(col, cell))) // Fehler
+  def rowSize(vecIndex: Int): Int = vector(vecIndex).size
+  def colSize(vecIndex: Int, row: Int) = vector(vecIndex)(row).size
+  def vector(vecIndex: Int) = this.productElement(vecIndex).asInstanceOf[Vector[Vector[Any]]]
