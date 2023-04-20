a1ad575a90aace5486ba91aeb96e0aef4161d264
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-04-20 21:47:50 +0200

    update field

--- src/main/scala/de/htwg/se/dotsandboxes/model/Field.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/model/Field.scala
@@ -5,25 +5,23 @@ import de.htwg.se.dotsandboxes.model.Matrix
 case class Field(matrix: Matrix[Any]):
     def this(row: Int, col: Int, filling: Any) = this(new Matrix(row, col, filling))
     def bar(length: Int = 7, cellNum: Int = 5, rowIndex: Int) = 
-        (0 until rowSize).map(xline(_, rowIndex, length)).mkString("O", "O", "O") + "\n"
-    def cells(row: Int, lenght: Int = 7, height: Int = 2) = 
-        (matrix.row(0, row).map(_.toString).map(" " * ((lenght - 1) / 2) + _ + " " * ((lenght - 1) / 2)).mkString("|", "|", "|") + "\n" ) * height
-    def mesh(lenght: Int = 7) =
-        //(0 until rowSize).map(bar(lenght, colSize, 0) + cells(0, lenght)).mkString("", "", bar(lenght, colSize, rowSize))
-        //(0 until rowSize).map(cells(_, lenght)).mkString(
-        //bar(lenght, colSize, 0), (1 until rowSize - 1).map(bar(lenght, colSize, _)), bar(lenght, colSize, rowSize))
-        val a = (0 until rowSize).map(bar(lenght, colSize, _)) // 3
-        //val b = (0 until rowSize).map(cells(_, lenght))     // 2
-        //(0 until rowSize).map((bar(lenght, colSize, i) + cells(i, lenght))).mkString("", "", a(rowSize))
-        //v.foreach{ case(i,j) => println(i, j) }
-        a.map(_ + cells(0, lenght)).mkString + bar(lenght, colSize, rowSize)
-        //a(0) + b(0) + a(1) + b(1) + a(2)
-        //(0 until rowSize).map(a(_)).mkString("", "", a(rowSize))
-    def put(vecIndex: Int, x: Int, y: Int, filling: Any) = 
+        (0 until cellNum).map(xline(rowIndex,_,length)).mkString(
+        Connectors.Dot.toString,Connectors.Dot.toString,Connectors.Dot.toString) + "\n"
+    def cells(row: Int, length: Int = 7, height: Int = 2) =
+        ((0 to colSize()).map(yline(row, _, length)).mkString + "\n") * height
+    def mesh(length: Int = 7) =
+        ((0 until rowSize()).map(x => bar(length, colSize(), x) + cells(x, length))).mkString + bar(length, colSize(), rowSize())
+    def xline(rowIndex: Int, lineIndex: Int, length: Int) =
+        if(matrix.cell(1, rowIndex, lineIndex).equals(false)) Connectors.EmptyX.toString * length 
+        else Connectors.ConnectedX.toString * length
+    def yline(rowIndex: Int, lineIndex: Int, length: Int) =
+        if(matrix.cell(2, rowIndex, lineIndex).equals(false)) Connectors.EmptyY.toString + filling(rowIndex, lineIndex, length)
+        else Connectors.ConnectedY.toString + filling(rowIndex, lineIndex, length)
+    def filling(rowIndex: Int, lineIndex: Int, length: Int) =
+        if(lineIndex < colSize()) space(length) + matrix.cell(0, rowIndex, lineIndex) + space(length) else Connectors.Empty.toString
+    def put(vecIndex: Int, x: Int, y: Int, filling: Any) =
         copy(matrix.replaceCell(vecIndex, x, y, filling))
+    def rowSize(row: Int = 0) = matrix.rowSize(row)
+    def colSize(row: Int = 0, col: Int = 0) = matrix.colSize(row, col)
+    def space(length: Int) = " " * ((length - 1) / 2)
     override def toString = mesh()
-    def xline(lineIndex: Int, rowIndex: Int, cellNumm: Int) =
-        if (matrix.cell(1, rowIndex, lineIndex).equals(false)) "-" * cellNumm else "=" * cellNumm
-
-    val rowSize = matrix.rowSize(0)
-    val colSize = matrix.colSize(0, 0)
