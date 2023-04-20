9be53ed27a2e84f677a330a38844786e826ddc13
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-04-20 21:48:20 +0200

    update matrix

--- src/main/scala/de/htwg/se/dotsandboxes/model/Matrix.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/model/Matrix.scala
@@ -1,15 +1,19 @@
 package de.htwg.se.dotsandboxes.model
 
-case class Matrix[T](vecFill: Vector[Vector[T]], vecX: Vector[Vector[Any]], vecY: Vector[Vector[Any]]):
+case class Matrix[T](vecFill: Vector[Vector[Any]], vecX: Vector[Vector[Any]], vecY: Vector[Vector[Any]]):
   def this(row: Int, col: Int, filling: T) = 
     this(Vector.tabulate(col, row) { (_, _) => filling}, 
     Vector.tabulate (col + 1, row) { (_, _) => false}, 
     Vector.tabulate (col, row + 1) { (_, _) => false})
-
+    
   def cell(vecIndex: Int, row: Int, col: Int): Any = vector(vecIndex)(row)(col)
   def row(vecIndex: Int, row: Int) = vector(vecIndex)(row)
   def replaceCell(vecIndex: Int, row: Int, col: Int, cell: Any): Matrix[Any] = 
-    copy(vecX = vector(vecIndex).updated(row, vector(vecIndex)(row).updated(col, cell))) // Fehler
+    vecIndex match
+      case 0 => copy(vecFill = vector(vecIndex).updated(row, vector(vecIndex)(row).updated(col, cell)))
+      case 1 => copy(vecX = vector(vecIndex).updated(row, vector(vecIndex)(row).updated(col, cell)))
+      case 2 => copy(vecY = vector(vecIndex).updated(row, vector(vecIndex)(row).updated(col, cell)))
+      case _ => copy()
   def rowSize(vecIndex: Int): Int = vector(vecIndex).size
   def colSize(vecIndex: Int, row: Int) = vector(vecIndex)(row).size
   def vector(vecIndex: Int) = this.productElement(vecIndex).asInstanceOf[Vector[Vector[Any]]]
