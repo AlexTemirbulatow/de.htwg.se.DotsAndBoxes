effdef90a4c0584f59a9a4eba740fe0e6e244788
Alextem <temirbulatow1@gmail.com>
2023-04-21 13:53:00 +0200

    continuous development

---
+++ src/test/scala/de/htwg/se/dotsandboxes/aview/TUISpec.scala
@@ -0,0 +1,12 @@
+package de.htwg.se.dotsandboxes
+package aview
+
+import controller.Controller
+import model.Field
+import model.Status
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+
+class TuiSpec extends AnyWordSpec {
+
+}
---
+++ src/test/scala/de/htwg/se/dotsandboxes/controller/ControllerSpec.scala
@@ -0,0 +1,12 @@
+package de.htwg.se.dotsandboxes
+package controller
+
+import model.Field
+import model.Status
+import util.Observer
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+
+class ControllerSpec extends AnyWordSpec {
+
+}
