fa15c4cb9d43d6d7bcf67341ad6193fba3a34562
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-04-27 22:14:05 +0200

    update field

--- src/main/scala/de/htwg/se/dotsandboxes/model/Field.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/model/Field.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.dotsandboxes.model
 
 case class Field(matrix: Matrix[Any]):
-    def this(row: Int, col: Int, filling: Any) = this(new Matrix(row, col, filling))
+    def this(row: Int, col: Int, status: Any) = this(new Matrix(row, col, status))
     def bar(length: Int = 7, cellNum: Int = 5, rowIndex: Int) = 
         (0 until cellNum).map(xline(rowIndex,_,length)).mkString(
         Connectors.Dot.toString,Connectors.Dot.toString,Connectors.Dot.toString) + "\n"
@@ -13,12 +13,14 @@ case class Field(matrix: Matrix[Any]):
         if(matrix.cell(1, rowIndex, lineIndex).equals(false)) Connectors.EmptyX.toString * length 
         else Connectors.ConnectedX.toString * length
     def yline(rowIndex: Int, lineIndex: Int, length: Int) =
-        if(matrix.cell(2, rowIndex, lineIndex).equals(false)) Connectors.EmptyY.toString + filling(rowIndex, lineIndex, length)
-        else Connectors.ConnectedY.toString + filling(rowIndex, lineIndex, length)
-    def filling(rowIndex: Int, lineIndex: Int, length: Int) =
+        if(matrix.cell(2, rowIndex, lineIndex).equals(false)) Connectors.EmptyY.toString + status(rowIndex, lineIndex, length)
+        else Connectors.ConnectedY.toString + status(rowIndex, lineIndex, length)
+    def status(rowIndex: Int, lineIndex: Int, length: Int) =
         if(lineIndex < colSize()) space(length) + matrix.cell(0, rowIndex, lineIndex) + space(length) else Connectors.Empty.toString
-    def put(vecIndex: Int, x: Int, y: Int, filling: Any) =
-        copy(matrix.replaceCell(vecIndex, x, y, filling))
+    def put(vecIndex: Int, x: Int, y: Int, status: Any) =
+        copy(matrix.replaceCell(vecIndex, x, y, status))
+    def check(vecIndex: Int, x: Int, y: Int): Field =
+        copy(matrix.checkMove(vecIndex, x, y))
     def get(vecIndex: Int, x: Int, y: Int) = matrix.cell(vecIndex, x, y)
     def rowSize(row: Int = 0) = matrix.rowSize(row)
     def colSize(row: Int = 0, col: Int = 0) = matrix.colSize(row, col)
