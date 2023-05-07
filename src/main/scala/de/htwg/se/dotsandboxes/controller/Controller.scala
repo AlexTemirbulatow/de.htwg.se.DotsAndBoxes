9db62544b49d00dd0eae326f4ece38600e37995d
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-05-07 20:26:27 +0200

    update Controller

--- src/main/scala/de/htwg/se/dotsandboxes/controller/Controller.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/controller/Controller.scala
@@ -5,22 +5,28 @@ import model.Field
 import model.Status
 import model.Move
 import model.Matrix
+import model.Player
 import util.Observable
 
 case class Controller(var field: Field) extends Observable:
-  def put(move: Move): Field =
-    field.put(move.vec, move.x, move.y, move.status)
+  override def toString: String = field.toString + "\n" + field.currentPlayer + "s turn\n[points: " + field.currentPoints + "]\n"
+  def put(move: Move): Field = field.putCell(move.vec, move.x, move.y, move.status)
+  def gameEnd = field.isFinished
+  def playerPoints = field.currentPoints
+  def winner = field.winner
+  def stats = field.stats
   def publish(doThis: Move => Field, move: Move): Unit =
     field = doThis(move)
-    field.checkEdge(move) match
+    val preStatus = field.currentStatus
+    field.isEdge(move) match
       case true  => field = field.doEdge(move.vec, move.x, move.y)
-      case false =>
-        move.vec match
-          case 1 =>
-            field = field.doMid(1, move.x, move.y)
-            field = field.doMid(2, move.x, move.y)
-          case 2 =>
-            field = field.doMid(3, move.x, move.y)
-            field = field.doMid(4, move.x, move.y)
+      case false => move.vec match
+        case 1 => field = field.doMid("downcase", move.x, move.y).doMid("upcase", move.x, move.y)
+        case 2 => field = field.doMid("rightcase", move.x, move.y).doMid("leftcase", move.x, move.y)
+    val postStatus = field.currentStatus
+    val difference = preStatus.indices.map(x => preStatus(x).zip(postStatus(x)).count(x => x._1 != x._2)).sum
+    field = difference match
+      case 0 => field.nextPlayer
+      case 1 => field.addPoint.updatePlayer
+      case 2 => field.addPoint.addPoint.updatePlayer
     notifyObservers
-  override def toString: String = field.toString
