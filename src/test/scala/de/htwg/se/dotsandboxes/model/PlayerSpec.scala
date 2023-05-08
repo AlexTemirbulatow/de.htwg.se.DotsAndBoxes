09f2cbb9bc1719dbfe6fa955d31efc46dbcf7c80
Alextem <temirbulatow1@gmail.com>
2023-05-08 20:51:03 +0200

    update PlayerSpec

--- src/test/scala/de/htwg/se/dotsandboxes/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/dotsandboxes/model/PlayerSpec.scala
@@ -10,7 +10,6 @@ class PlayerSpex extends AnyWordSpec {
                 val player = Player
                 player.list should be(Vector(Player("Blue", 0, Status.Blue), Player("Red", 0, Status.Red),
                     Player("Green", 0, Status.Green), Player("Yellow", 0, Status.Yellow)))
-                player.list should contain(Player("Green", 0, Status.Green))
                 player.list shouldBe a[Vector[Player]]
                 player.list.size should be(4)
 
@@ -25,6 +24,7 @@ class PlayerSpex extends AnyWordSpec {
             "return the correct player initials" in {
                 val player1 = Player("Player1", 10, Status.Blue)
                 val player2 = Player(playerId = "Player2", status = Status.Yellow)
+                val player3 = Player(playerId = "Player3", points = 5, status = Status.Green)
 
                 player1.playerId should be("Player1")
                 player1.points should be(10)
@@ -34,6 +34,9 @@ class PlayerSpex extends AnyWordSpec {
                 player2.points should be(0)
                 player2.status should be(Status.Yellow)
 
+                player3.playerId should be("Player3")
+                player3.points should be(5)
+                player3.status should be(Status.Green)
 
                 val list = Player.list
 
@@ -52,6 +55,23 @@ class PlayerSpex extends AnyWordSpec {
                 list(3).playerId should be("Yellow")
                 list(3).points should be(0)
                 list(3).status should be(Status.Yellow)
+
+
+                playerBlue.playerId should be("Blue")
+                playerBlue.points should be(0)
+                playerBlue.status should be(Status.Blue)
+
+                playerRed.playerId should be("Red")
+                playerRed.points should be(0)
+                playerRed.status should be(Status.Red)
+
+                playerGreen.playerId should be("Green")
+                playerGreen.points should be(0)
+                playerGreen.status should be(Status.Green)
+
+                playerYellow.playerId should be("Yellow")
+                playerYellow.points should be(0)
+                playerYellow.status should be(Status.Yellow)
 
             }
         }
