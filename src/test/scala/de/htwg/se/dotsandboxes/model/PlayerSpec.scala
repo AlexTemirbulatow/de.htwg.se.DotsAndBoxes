966cf9cd7878b8599f63427c9ae1a2822ef7350f
Alextem <temirbulatow1@gmail.com>
2023-05-08 20:05:49 +0200

    update PlayerSpec

--- src/test/scala/de/htwg/se/dotsandboxes/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/dotsandboxes/model/PlayerSpec.scala
@@ -8,9 +8,11 @@ class PlayerSpex extends AnyWordSpec {
         "accessing list" should {
             "return the correct player and list" in {
                 val player = Player
-                player.list should be(Vector(Player("Blue", 0, Status.Blue), Player("Red", 0, Status.Red), Player("Green", 0, Status.Green), Player("Yellow", 0, Status.Yellow)))
+                player.list should be(Vector(Player("Blue", 0, Status.Blue), Player("Red", 0, Status.Red),
+                    Player("Green", 0, Status.Green), Player("Yellow", 0, Status.Yellow)))
                 player.list should contain(Player("Green", 0, Status.Green))
                 player.list shouldBe a[Vector[Player]]
+                player.list.size should be(4)
 
                 player.list.head should be(Player("Blue", 0, Status.Blue))
                 player.list.last should be(Player("Yellow", 0, Status.Yellow))
@@ -19,19 +21,38 @@ class PlayerSpex extends AnyWordSpec {
                 player.list(1) should be(Player("Red", 0, Status.Red))
                 player.list(2) should be(Player("Green", 0, Status.Green))
                 player.list(3) should be(Player("Yellow", 0, Status.Yellow))
+            }
+            "return the correct player initials" in {
+                val player1 = Player("Player1", 10, Status.Blue)
+                val player2 = Player(playerId = "Player2", status = Status.Yellow)
 
-                val player1 = new Player("Player1", 10, Status.Empty)
-                val player2 = Player(playerId = "Player2", status = Status.Empty)
-
+                player1.playerId should be("Player1")
                 player1.points should be(10)
+                player1.status should be(Status.Blue)
+
+                player2.playerId should be("Player2")
                 player2.points should be(0)
+                player2.status should be(Status.Yellow)
 
 
                 val list = Player.list
 
-                list.size should be(4)
-                list shouldBe a[Vector[Player]]
-                list(0) should be(Player("Blue", 0, Status.Blue))
+                list(0).playerId should be("Blue")
+                list(0).points should be(0)
+                list(0).status should be(Status.Blue)
+
+                list(1).playerId should be("Red")
+                list(1).points should be(0)
+                list(1).status should be(Status.Red)
+
+                list(2).playerId should be("Green")
+                list(2).points should be(0)
+                list(2).status should be(Status.Green)
+
+                list(3).playerId should be("Yellow")
+                list(3).points should be(0)
+                list(3).status should be(Status.Yellow)
+
             }
         }
         "creating a player list" should {
