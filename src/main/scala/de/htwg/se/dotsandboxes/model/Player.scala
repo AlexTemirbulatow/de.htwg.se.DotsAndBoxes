0337fc257e15c67b45c3a34460a149af96c00ee7
Alextem <temirbulatow1@gmail.com>
2023-05-08 21:46:33 +0200

    update Player

--- src/main/scala/de/htwg/se/dotsandboxes/model/Player.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/model/Player.scala
@@ -1,12 +1,11 @@
 package de.htwg.se.dotsandboxes.model
 
 case class Player(playerId: String, points: Int = 0, status: Status)
-val playerBlue = Player("Blue", 0, Status.Blue)
-val playerRed = Player("Red", 0, Status.Red)
-val playerGreen = Player("Green", 0, Status.Green)
-val playerYellow = Player("Yellow", 0, Status.Yellow)
 object Player:
-    val list = Vector(playerBlue, playerRed, playerGreen, playerYellow)
-
+    val list = Vector(
+        Player("Blue", 0, Status.Blue),
+        Player("Red", 0, Status.Red),
+        Player("Green", 0, Status.Green),
+        Player("Yellow", 0, Status.Yellow))
 case class PlayerList(playerList: Vector[Player]):
     def this(playerSize: Int = 2) = this(Vector.tabulate(playerSize)(x => Player.list(x)))
