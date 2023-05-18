f15c67e3a00cb395816f97386b382142737b5ee5
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-05-18 20:56:03 +0200

    update TUI

--- src/main/scala/de/htwg/se/dotsandboxes/aview/TUI.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/aview/TUI.scala
@@ -5,27 +5,47 @@ import scala.io.StdIn.readLine
 import controller.Controller
 import model.Move
 import util.Observer
+import util.GameState
 
 class TUI(controller: Controller) extends Template(controller):
     override def update = println("\n" + controller.toString)
 
     override def gameLoop: Unit =
-        if(controller.gameEnd) println(finalStats)
-        analyseInput(readLine) match
-            case None => print("\naborted\n")
-            case Some(move) => controller.publish(controller.put, move)
+        controller.handle(checkState.handle(controller.gameEnd))
         gameLoop
 
-    override def analyseInput(input: String) = input match
-        case "q" => None
-        case _   => val chars = input.toCharArray
-            val line = chars(0).toString.toInt
-            val x    = chars(1).toString.toInt
-            val y    = chars(2).toString.toInt
-            Some(Move(line, x, y, true))
+    object checkState:
+        var state = GameState.Running
+        def handle(check: Boolean): GameState = 
+            state = check match
+                case true  => finished
+                case false => analyseInput(readLine)
+            state
+        def finished: GameState =
+            remove 
+            println(finalStats)
+            GameState.Finished
+        def analyseInput(input: String): GameState = input match
+            case "q" =>
+                println(aborted)
+                remove
+                GameState.Aborted
+            case _   => val chars = input.toCharArray
+                val line = chars(0).toString.toInt
+                val x    = chars(1).toString.toInt
+                val y    = chars(2).toString.toInt
+                controller.publish(controller.put, Move(line, x, y, true))
+                GameState.Running
 
     override def finalStats =
         controller.stats + "\n\n" +
         "_________________________" + "\n\n" +
         controller.winner +
         "\n"
+
+    override def aborted =
+        "\nAborted\n"
+
+    override def remove = 
+        controller.remove(this)
+        false 
