0cfc30e9a35ed6f0ca6d887fc042e85aeef6e284
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-05-18 17:40:01 +0200

    update ConSpec

--- src/test/scala/de/htwg/se/dotsandboxes/model/ConnectorsSpec.scala
+++ src/test/scala/de/htwg/se/dotsandboxes/model/ConnectorsSpec.scala
@@ -7,18 +7,18 @@ class ConnectorsSpec extends AnyWordSpec {
     "Connectores" when {
         "accessing" should {
             "have the right string representation" in {
-                val dot = Connectors.Dot
-                dot.toString should be("O")
-                val emptyRow = Connectors.EmptyRow
-                emptyRow.toString should be("-")
-                val connectedRow = Connectors.ConnectedRow
-                connectedRow.toString should be("=")
-                val emptyColumn = Connectors.EmptyColumn
-                emptyColumn.toString should be("¦")
-                val connectedColumn = Connectors.ConnectedColumn
-                connectedColumn.toString should be("‖")
-                val empty = Connectors.Empty
-                empty.toString should be("")
+                val dot = Connectors("O")
+                dot should be("O")
+                val emptyRow = Connectors("-")
+                emptyRow should be("-")
+                val connectedRow = Connectors("=")
+                connectedRow should be("=")
+                val emptyColumn = Connectors("¦")
+                emptyColumn should be("¦")
+                val connectedColumn = Connectors("‖")
+                connectedColumn should be("‖")
+                val empty = Connectors("")
+                empty should be("")
             }
         }
     }
