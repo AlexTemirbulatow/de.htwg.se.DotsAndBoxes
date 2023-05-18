daea0ee7c8c34dea75208b44be8777508d69fcc5
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-05-18 17:33:44 +0200

    update TUI

--- src/main/scala/de/htwg/se/dotsandboxes/aview/TUI.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/aview/TUI.scala
@@ -3,49 +3,29 @@ package aview
 
 import scala.io.StdIn.readLine
 import controller.Controller
-import util.Observer
 import model.Move
+import util.Observer
 
-class TUI(controller: Controller) extends Observer:
-    controller.add(this)
+class TUI(controller: Controller) extends Template(controller):
     override def update = println("\n" + controller.toString)
 
-    def welcome =
-        "\n" +
-        "---------------------------------" + "\n" +
-        "¦ Welcome to Dots and Boxes TUI ¦" + "\n" +
-        "---------------------------------" + "\n" +
-        "\n"
-
-    def finished =
-        "\n" +
-        "-----------------" + "\n" +
-        "| Game finished |" + "\n" +
-        "-----------------" + "\n" +
-        "\n"
-
-    def finalStats =
-        controller.stats + "\n\n" +
-        "_________________________" + "\n\n" +
-        controller.winner +
-        "\n"
-
-    def run =
-        println(welcome)
-        println(controller.toString)
-        gameLoop
-
-    def gameLoop: Unit =
-        if(controller.gameEnd) println(finished + finalStats)
+    override def gameLoop: Unit =
+        if(controller.gameEnd) println(finalStats)
         analyseInput(readLine) match
-            case None => print(finished)
+            case None => print("\naborted\n")
             case Some(move) => controller.publish(controller.put, move)
         gameLoop
 
-    def analyseInput(input: String) = input match
+    override def analyseInput(input: String) = input match
         case "q" => None
         case _   => val chars = input.toCharArray
             val line = chars(0).toString.toInt
             val x    = chars(1).toString.toInt
             val y    = chars(2).toString.toInt
             Some(Move(line, x, y, true))
+
+    override def finalStats =
+        controller.stats + "\n\n" +
+        "_________________________" + "\n\n" +
+        controller.winner +
+        "\n"
