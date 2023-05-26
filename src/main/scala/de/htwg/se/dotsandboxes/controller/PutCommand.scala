fd6347a5386be8103f27ff2130503561bf4afd30
Alextem <temirbulatow1@gmail.com>
2023-05-26 11:09:44 +0200

    further development

--- src/main/scala/de/htwg/se/dotsandboxes/controller/PutCommand.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/controller/PutCommand.scala
@@ -6,7 +6,5 @@ import util.{Command, UndoManager}
 
 class PutCommand(move: Move) extends Command:
   override def doStep(field: Field): Field = field.putCell(move.vec, move.x, move.y, move.status)
-  override def undoStep(field: Field): Field =
-    field.putCell(move.vec, move.x, move.y, false)
-  override def redoStep(field: Field): Field =
-    field.putCell(move.vec, move.x, move.y, move.status)
+  override def undoStep(field: Field): Field = field.putCell(move.vec, move.x, move.y, false)
+  override def redoStep(field: Field): Field = field.putCell(move.vec, move.x, move.y, move.status)
