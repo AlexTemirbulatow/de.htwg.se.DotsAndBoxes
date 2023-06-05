744db5f899b99f91b91e00f7915586fcace1a7ad
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-06-05 22:53:58 +0200

    update MoveStratagy

--- src/main/scala/de/htwg/se/dotsandboxes/controller/controllerComponent/MoveStratagy.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/controller/controllerComponent/MoveStratagy.scala
@@ -5,6 +5,6 @@ import model.{Field, Move}
 import model.MoveState.{EdgeState, MidState}
 
 /* strategy pattern */
-  object StrategyMove:
-    def decideMove(move: Move, field: Field): Field =
-      if(field.isEdge(move)) EdgeState.handle(move, field) else MidState.handle(move, field)
+object StrategyMove:
+  def decideMove(move: Move, field: Field): Field =
+    if(field.isEdge(move)) EdgeState.handle(move, field) else MidState.handle(move, field)
