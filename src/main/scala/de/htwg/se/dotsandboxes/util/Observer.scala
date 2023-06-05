87f5a45834fc5f339d8b3773e79e786e4698a3af
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-06-05 22:59:37 +0200

    update Observer

--- src/main/scala/de/htwg/se/dotsandboxes/util/Observer.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/util/Observer.scala
@@ -1,10 +1,15 @@
 package de.htwg.se.dotsandboxes
 package util
 
-trait Observer { def update: Unit }
+trait Observer { def update(e: Event): Unit }
 
 trait Observable:
   var subscribers: Vector[Observer] = Vector()
   def add(s: Observer) = subscribers = subscribers :+ s
   def remove(s: Observer) = subscribers = subscribers.filterNot(o => o == s)
-  def notifyObservers = subscribers.foreach(o => o.update)
+  def notifyObservers(e: Event) = subscribers.foreach(o => o.update(e))
+
+enum Event:
+  case Abort
+  case Move
+  case End
