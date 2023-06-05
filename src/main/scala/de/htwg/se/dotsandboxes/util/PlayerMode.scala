9b132ddf55befb9e6a9dd46c8991a593c94cc06d
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-06-05 22:59:53 +0200

    update PlayerMode

--- src/main/scala/de/htwg/se/dotsandboxes/util/PlayerMode.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/util/PlayerMode.scala
@@ -5,9 +5,13 @@ import controller.Controller
 import model.{Field, Status}
 import scala.io.StdIn.readLine
 
-/*Strategy pattern*/
+/* strategy pattern */
 object PlayerMode:
-    var selectPlayerMode = getInput
+    def twoPlayers: Field = new Field(5, 4, Status.Empty, 2)
+    def threePlayers: Field = new Field(8, 6, Status.Empty, 3)
+    def fourPlayers: Field = new Field(11, 9, Status.Empty, 4)
+
+    val selectPlayerMode = getInput
     def getInput: Field =
         print(
             "\n" +
@@ -16,14 +20,8 @@ object PlayerMode:
             "---------------------------------" + "\n" +
             "\n")
         val input = readLine("\nChoose playersize (2-4): ")
-        selectPlayerMode = input match
+        input match
             case "2" => twoPlayers
             case "3" => threePlayers
             case "4" => fourPlayers
             case _   => twoPlayers
-        selectPlayerMode
-    def twoPlayers = new Field(5, 4, Status.Empty, 2)
-    def threePlayers = new Field(8, 6, Status.Empty, 3)
-    def fourPlayers = new Field(11, 9, Status.Empty, 4)
-
-    
