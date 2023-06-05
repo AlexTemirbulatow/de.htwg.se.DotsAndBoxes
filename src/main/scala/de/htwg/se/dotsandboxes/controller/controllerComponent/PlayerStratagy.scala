b7d0854bffb73088e8e39ca751bf2565159742bd
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-06-05 22:54:26 +0200

    update Playerstratagy

--- src/main/scala/de/htwg/se/dotsandboxes/controller/controllerComponent/PlayerStratagy.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/controller/controllerComponent/PlayerStratagy.scala
@@ -4,9 +4,10 @@ package controller.controllerComponent
 import model.Field
 import model.PlayerState.{AddOnePoint, AddTwoPoints, NextPlayer}
 
-  object StrategyPlayer:
-    def updatePlayer(field: Field, preStatus: Vector[Vector[Any]], postStatus: Vector[Vector[Any]]): Field =
-      val difference = preStatus.indices.map(x => preStatus(x).zip(postStatus(x)).count(x => x._1 != x._2)).sum
-      if(difference.equals(1)) AddOnePoint.handle(field)
-      else if(difference.equals(2)) AddTwoPoints.handle(field)
-      else NextPlayer.handle(field)
+/* stratagy pattern */
+object StrategyPlayer:
+  def updatePlayer(field: Field, preStatus: Vector[Vector[Any]], postStatus: Vector[Vector[Any]]): Field =
+    val difference = preStatus.indices.map(x => preStatus(x).zip(postStatus(x)).count(x => x._1 != x._2)).sum
+    if(difference.equals(1)) AddOnePoint.handle(field)
+    else if(difference.equals(2)) AddTwoPoints.handle(field)
+    else NextPlayer.handle(field)
