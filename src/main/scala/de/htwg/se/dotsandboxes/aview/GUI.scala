49bfc7e08ddb8a3a6f0c7bdfd6dce8c1c512404e
Alextem <temirbulatow1@gmail.com>
2023-06-06 21:57:07 +0200

    further development

--- src/main/scala/de/htwg/se/dotsandboxes/aview/GUI.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/aview/GUI.scala
@@ -7,29 +7,35 @@ import util.{Observer, Event}
 
 import scala.swing.*
 import scala.swing.event.*
-import javax.swing.ImageIcon
-import javax.imageio.ImageIO
 import java.io.File
+import javax.imageio.ImageIO
+import javax.swing.ImageIcon
 import java.awt.Color
+import javax.swing.UIManager
 
 class GUI(controller: Controller) extends Frame with Observer:
     controller.add(this)
 
     val fieldSize: (Int, Int) = (controller.colSize(1, 0), controller.rowSize(2))
     val grid: (Int, Int) = ((fieldSize._1 + fieldSize._1 + 1), (fieldSize._2 + fieldSize._2 + 1))
-
+    
     title = "Dots And Boxes"
-    iconImage = ImageIO.read(new File("src/resources/3_Ikon.png"))
+    iconImage = ImageIO.read(new File("src/resources/4_Ikon.png"))
     menuBar = new MenuBar {
-        contents += new Menu("Settings") {
+        contents += new Menu("") {
+            icon = new ImageIcon(ImageIO.read(new File("src/resources/3_Menu.png")))
+            borderPainted = false
             contents += new MenuItem(Action("Exit") { controller.abort })
             contents += new MenuItem(Action("Undo") { controller.publish(controller.undo) })
             contents += new MenuItem(Action("Redo") { controller.publish(controller.redo) })
         }
     }
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName)
+    menuBar.border = Swing.EmptyBorder(0, 10, 0, 0)
+    menuBar.background = Color(240, 240, 240)
     update(Event.Move)
-    pack
     centerOnScreen
+    pack
     open()
 
     override def update(event: Event): Unit = event match
@@ -40,7 +46,7 @@ class GUI(controller: Controller) extends Frame with Observer:
     override def closeOperation = controller.abort
 
     def handle = new BorderPanel {
-        val dim = new Dimension(850, 650)
+        val dim = new Dimension(900, 750)
         minimumSize = dim
         maximumSize = dim
         preferredSize = dim
@@ -50,8 +56,8 @@ class GUI(controller: Controller) extends Frame with Observer:
         label.font = new Font("Comic Sans MS", 0, 30)
         val stats = new TextArea(controller.stats.replace("\n", "   |   "))
         stats.font = new Font("Comic Sans MS", 0, 17)
-        stats.background = new Color(220, 220, 220)
-        stats.border = Swing.EmptyBorder(0, 10, 0, 0)
+        stats.background = Color(220, 220, 220)
+        stats.border = Swing.EmptyBorder(0, 35, 0, 0)
         stats.editable = false
         add(label, BorderPanel.Position.North)
         add(new CellPanel(fieldSize._1, fieldSize._2), BorderPanel.Position.Center)
@@ -90,16 +96,15 @@ class GUI(controller: Controller) extends Frame with Observer:
     class CellButton(vec: Int, x: Int, y: Int, status: Boolean) extends Button:
         borderPainted = false
         focusPainted = false
-        focusable = false
-        background = Color.WHITE
+        opaque = false
 
-        vec match
+        icon = vec match
             case 1 => status match
-                case true => icon = new ImageIcon(ImageIO.read(new File("src/resources/1_BarTaken.png")))
-                case false => icon = new ImageIcon(ImageIO.read(new File("src/resources/2_White.png")))
+                case true => new ImageIcon(ImageIO.read(new File("src/resources/1_BarTaken.png")))
+                case false => new ImageIcon(ImageIO.read(new File("src/resources/2_White.png")))
             case 2 => status match
-                case true => icon = new ImageIcon(ImageIO.read(new File("src/resources/1_ColTaken.png")))
-                case false => icon = new ImageIcon(ImageIO.read(new File("src/resources/2_White.png")))
+                case true => new ImageIcon(ImageIO.read(new File("src/resources/1_ColTaken.png")))
+                case false => new ImageIcon(ImageIO.read(new File("src/resources/2_White.png")))
 
         listenTo(mouse.moves, mouse.clicks)
         reactions += {
@@ -116,13 +121,12 @@ class GUI(controller: Controller) extends Frame with Observer:
 
             case MouseExited(source) => status match
                 case true =>
-                case false => icon = new ImageIcon(ImageIO.read(new File("src/resources/2_White.png")))
-        }
+                case false => icon = new ImageIcon(ImageIO.read(new File("src/resources/2_White.png")))}
 
 
     class Scoreboard extends Frame:
         title = "Scoreboard"
-        iconImage = ImageIO.read(new File("src/resources/3_Ikon.png"))
+        iconImage = ImageIO.read(new File("src/resources/4_Ikon.png"))
         contents = new BorderPanel {
             val dim = new Dimension(300, 200)
             minimumSize = dim
