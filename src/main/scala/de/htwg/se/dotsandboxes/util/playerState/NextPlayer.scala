06be87ce18d3b8ed93281d782590c77d5ac4efe5
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-06-16 09:51:24 +0200

    update NextPlayer

--- src/main/scala/de/htwg/se/dotsandboxes/util/playerState/NextPlayer.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/util/playerState/NextPlayer.scala
@@ -3,5 +3,6 @@ package util.playerState
 
 import model.fieldComponent.FieldInterface
 
+
 object NextPlayer extends PlayerState:
     override def handle(field: FieldInterface): FieldInterface = field.nextPlayer
