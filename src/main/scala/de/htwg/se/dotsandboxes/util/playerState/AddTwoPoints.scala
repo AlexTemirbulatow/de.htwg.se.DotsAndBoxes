f3cf45be8810860179b4fa3b3d156d4769acbdeb
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-06-16 09:51:01 +0200

    update AddTwoPoints

--- src/main/scala/de/htwg/se/dotsandboxes/util/playerState/AddTwoPoints.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/util/playerState/AddTwoPoints.scala
@@ -3,5 +3,6 @@ package util.playerState
 
 import model.fieldComponent.FieldInterface
 
+
 object AddTwoPoints extends PlayerState:
     override def handle(field: FieldInterface): FieldInterface = field.addPoints(2).updatePlayer
