b7c91c08625117724d0c2e1e4fa663f33c34112d
Yazanski <116159892+Yazanski@users.noreply.github.com>
2023-06-22 19:40:37 +0200

    update Controller

--- src/main/scala/de/htwg/se/dotsandboxes/controller/controllerComponent/controllerImpl/Controller.scala
+++ src/main/scala/de/htwg/se/dotsandboxes/controller/controllerComponent/controllerImpl/Controller.scala
@@ -10,9 +10,10 @@ import util._
 import model.fieldComponent.FieldInterface
 import model.fieldComponent.fieldImpl.Move
 import model.matrixComponent.matrixImpl.Player
+import model.fileIoComponent.FileIOInterface
 
 
-class Controller(using var field: FieldInterface) extends ControllerInterface:
+class Controller(using var field: FieldInterface, val fileIO: FileIOInterface) extends ControllerInterface:
   /* setup chain */
   val moveCheck_Available = CheckAvailable(None)
   val moveCheck_Y = CheckY(Some(moveCheck_Available))
@@ -24,14 +25,21 @@ class Controller(using var field: FieldInterface) extends ControllerInterface:
 
   override def put(move: Move): FieldInterface = undoManager.doStep(field, PutCommand(move, field))
   override def get(vec: Int, x: Int, y: Int): Any = field.getCell(vec, x, y)
+  override def abort: Unit = notifyObservers(Event.Abort)
   override def undo: FieldInterface = undoManager.undoStep(field)
   override def redo: FieldInterface = undoManager.redoStep(field)
-  override def abort: Unit = notifyObservers(Event.Abort)
+  override def save: Unit =
+    fileIO.save(field)
+    if !gameEnded then notifyObservers(Event.Move)
+  override def load: Unit = 
+    field = fileIO.load
+    notifyObservers(Event.Move)
+    if gameEnded then notifyObservers(Event.End)
 
   override def colSize(row: Int = 0, col: Int = 0): Int = field.colSize(row, col)
   override def rowSize(row: Int = 0): Int = field.rowSize(row)
   override def playerList: Vector[Player] = field.playerList
-  override def currentPlayer: String = field.currentPlayer
+  override def currentPlayer: String = field.currentPlayerId
   override def currentPoints: Int = field.currentPoints
   override def gameEnded: Boolean = field.isFinished
   override def winner: String = field.winner
@@ -40,6 +48,7 @@ class Controller(using var field: FieldInterface) extends ControllerInterface:
   override def publish(doThis: => FieldInterface): Unit =
     field = doThis
     notifyObservers(Event.Move)
+    if gameEnded then notifyObservers(Event.End)
   override def publish(doThis: Move => FieldInterface, move: Move): Unit = moveCheck_Line.handle(move, field) match
     case Failure(exception) => print(exception.getMessage.dropRight(28))
     case Success(value) =>
