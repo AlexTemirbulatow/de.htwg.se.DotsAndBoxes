7c6d52a2bec5dfa5924d5b736ad5c23fc64a84c2
Alextem <temirbulatow1@gmail.com>
2023-06-22 22:37:24 +0200

    further development

--- src/test/scala/de/htwg/se/dotsandboxes/model/fileIoComponent/xmlImpl/FileIoSpec.scala
+++ src/test/scala/de/htwg/se/dotsandboxes/model/fileIoComponent/xmlImpl/FileIoSpec.scala
@@ -10,10 +10,9 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
 
-
 class FileIoSpec extends AnyWordSpec {
     "A field" when {
-        val field: Field = new Field(2, 2, Status.Empty, 2)
+        val field: Field = new Field(5, 4, Status.Empty, 2)
         "saved to xml" should {
             val fileIO = new FileIO()
             "be equal when loaded" in {
